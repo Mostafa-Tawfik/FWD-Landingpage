06744f2850407bc75874b6b1e6fca92b350aef83
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2021-11-21 18:52:07 +0200

    feat: add class to active section, refactor: more clear codes & var names
    - feat: add a unique class to be clear which section is being viewed while scrolling through the page.
    
    -refactor: change var names so it is easy to read.
    
    -fix: repair the nav menu to match the styling and include href links.

--- js/app.js
+++ js/app.js
@@ -41,34 +41,46 @@ const frag = document.createDocumentFragment();
 */
 
 // build the nav
-sections.forEach(function(elem) {
-    const newList = document.createElement('li');
+sections.forEach((elem) => {
+    const li = document.createElement('li');
     const text = elem.getAttribute('data-nav');
-    const newLink = document.createElement('a');
-    newLink.textContent = text;
-    newList.appendChild(newLink);
+    const anchor = document.createElement('a');
+    const anchorText = document.createTextNode(text);
+    anchor.appendChild(anchorText);
+    anchor.classList.add('menu__link');
+    anchor.href = '#text';
+    li.appendChild(anchor);
 
     // scroll into view
-    newList.addEventListener('click',() => {
-            elem.scrollIntoView({
-                behavior: 'smooth',
-                block: 'center'
-            });
-        })
+    anchor.addEventListener('click', () => {
+        elem.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    });
 
     // style the navbar
-    newList.setAttribute('style','color: black; padding: 1rem; font-weight:bold; float: left');
-    
+    li.setAttribute('style', 'color: black; padding: 1rem; font-weight:bold; float: left');
+
     // append to Fragment
-    frag.appendChild(newList);
+    frag.appendChild(li);
 });
 
 // append to navbar__list
 navbarList.appendChild(frag);
 
 // Add class 'active' to section when near top of viewport
-
-
+function switchToActiveSection (){
+    sections.forEach(active => {
+        const position = active.getBoundingClientRect();
+        if (position.top >= 0 && position.bottom <= window.innerHeight){
+                sections.forEach(notActive => {
+                    notActive.classList.remove('your-active-class');
+                });
+                active.classList.add('your-active-class');
+            };
+    });
+};
 // Scroll to anchor ID using scrollTO event
 
 
@@ -83,5 +95,5 @@ navbarList.appendChild(frag);
 // Scroll to section on link click
 
 // Set sections as active
-
+window.addEventListener('scroll', switchToActiveSection);
 
